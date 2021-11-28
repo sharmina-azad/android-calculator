e7220ec5269662279db6af7bca39c947eb5345b6
sharmina-azad <azad1707093@stud.kuet.ac.bd>
2021-11-28 23:42:39 +0600

    add a new java class

---
+++ app/src/main/java/zone/eloy/projects/androidcalculator/mishu.java
@@ -0,0 +1,19 @@
+package zone.eloy.projects.androidcalculator;
+
+public class mishu {
+
+    int x;
+
+    public mishu(int x) {
+
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
